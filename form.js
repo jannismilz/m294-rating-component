e10e3ce5e638fa036198812ae2f33958f22edf9b
Jannis Milz <jannis@milz.ch>
2023-08-22 14:02:46 +0200

    Create form handler and add form submit handling

---
+++ form.js
@@ -0,0 +1,45 @@
+import { storeRating } from "./storage.js";
+
+const form = document.getElementById("yourrating");
+const nameInput = document.querySelector("#yourrating [name='name']");
+const nameInputErr = document.querySelector("#nameErr");
+const ratingInput = document.querySelector("#yourrating [name='rating']");
+const ratingInputErr = document.querySelector("#ratingErr");
+const reviewInput = document.querySelector("#yourrating [name='review']");
+const reviewInputErr = document.querySelector("#reviewErr");
+const submitBtn = document.querySelector("#yourrating [name='submit']");
+const submitSuccess = document.querySelector("#submitSuccess");
+
+function handleFormSubmit(el) {
+    if (el.preventDefault) el.preventDefault();
+
+    let error = false;
+    if (!nameInput.value) {
+        nameInputErr.innerText = "Dieses Feld muss ausgefüllt werden!";
+        error = true;
+    }
+    if (!ratingInput.value) {
+        ratingInputErr.innerText = "Dieses Feld muss ausgefüllt werden!";
+        error = true;
+    }
+    if (!reviewInput.value) {
+        reviewInputErr.innerText = "Dieses Feld muss ausgefüllt werden!";
+        error = true;
+    }
+
+    if (error) return;
+
+    storeRating(nameInput.value, ratingInput.value, reviewInput.value);
+
+    // Reset all fields
+    nameInput.value = "";
+    ratingInput.value = "";
+    reviewInput.value = "";
+    submitSuccess.innerText = "Bewertung erfolgreich abgesendet!";
+}
+
+if (form.attachEvent) {
+    form.attachEvent("submit", handleFormSubmit);
+} else {
+    form.addEventListener("submit", handleFormSubmit);
+}
