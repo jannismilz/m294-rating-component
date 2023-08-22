99a42a71b157342f4591b036af3415bf481764cb
Jannis Milz <jannis@milz.ch>
2023-08-22 16:39:10 +0200

    Add sort functionality

--- sort.js
+++ sort.js
@@ -1,1 +1,10 @@
-const sortSelect = document.getElementById("");
+import { rerenderRatings } from "./ratings.js";
+
+const sortSelect = document.getElementById("ratingsSort");
+
+sortSelect.addEventListener("change", () => {
+    const value = sortSelect.value;
+    const [sort, sortType] = value.split("-"); // Format: publish-desc
+
+    rerenderRatings(sort, sortType);
+});
